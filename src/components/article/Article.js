2d06d9b9e28350b6cfcdcf659446c221e92a7982
Nikothaboss <57292271+Nikothaboss@users.noreply.github.com>
2021-08-31 13:05:41 +0200

    removed bloat

---
+++ src/components/article/Article.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Article = () => {
+    return (
+        <article>
+            <h2>Headline</h2>
+            <p>Body</p>
+            <span>Author</span>
+        </article>
+    )
+}
+
+export default Article
